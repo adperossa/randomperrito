1736e6e2cf4634d642ae942d68f5352481008c72
Axel Perossa <adperossa@gmail.com>
2020-04-09 19:58:28 -0300

    Updates

---
+++ precache-manifest.148daf3cf8d6d0849c27049879e7de06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fa2fd3f1bf11c78569e61abb855758",
+    "url": "/randomperrito/index.html"
+  },
+  {
+    "revision": "fc2a16a321e01ef9b5c9",
+    "url": "/randomperrito/static/css/main.d0061d4e.chunk.css"
+  },
+  {
+    "revision": "c0f389c19a0afef7d274",
+    "url": "/randomperrito/static/js/2.d05075f0.chunk.js"
+  },
+  {
+    "revision": "fc2a16a321e01ef9b5c9",
+    "url": "/randomperrito/static/js/main.c333b61b.chunk.js"
+  },
+  {
+    "revision": "016f567033ef6b0009c7",
+    "url": "/randomperrito/static/js/runtime-main.a2f394bd.js"
+  },
+  {
+    "revision": "b31deb3e2c83b178f1a60ee2db440583",
+    "url": "/randomperrito/static/media/logo.b31deb3e.svg"
+  }
+]);
